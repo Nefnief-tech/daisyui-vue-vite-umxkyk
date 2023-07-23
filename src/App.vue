f5e48653f945f5a2f2fcf156cac9e97b3a41660e
Nefnief-tech <104936980+Nefnief-tech@users.noreply.github.com>
2023-07-23 18:14:52 +0200

    1.1 v2

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="hero min-h-screen bg-base-200">
     <div class="hero-content flex-col lg:flex-row-reverse">
-      <img
-        src="/images/stock/photo-1635805737707-575885ab0820.jpg"
-        class="max-w-sm rounded-lg shadow-2xl"
-      />
-      <div>
-        <h1 class="text-5xl font-bold">DR. Ulrike Hirsch & Elke Brueggemann</h1>
-        <p class="py-6">
-          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
-          id nisi.
+      <div class="text-center lg:text-left">
+        <h1 class="text-5xl font-bold pl-5">Start NOW</h1>
+        <p class="py-6 pl-5">
+          Start now with ultrafast and secure hosting with premium locations based in secure datacenters in Germany, France, Netherlands, United Kingdom, United States, Canada, Singapore, Australia, and India.
         </p>
-        <button class="btn btn-primary">Erfahre Mehr</button>
+      </div>
+      <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <div class="card-body">
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              placeholder="email"
+              class="input input-bordered"
+            />
+          </div>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Password</span>
+            </label>
+            <input
+              type="text"
+              placeholder="password"
+              class="input input-bordered"
+            />
+            <label class="label">
+              <a href="#" class="label-text-alt link link-hover"
+                >Forgot password?</a
+              >
+            </label>
+          </div>
+          <div class="form-control mt-6">
+            <button class="btn btn-primary">Login</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
